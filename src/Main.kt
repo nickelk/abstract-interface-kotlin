04953e0616603bd4b50f111811fcaf199b6092d0
nickelk <nick_smith@ukr.net>
2020-08-30 10:44:41 +0300

    second commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
     val user = MainProvider()
-    user.printInfo(User())
-    println(user.getConnection())
+//    user.printInfo(User())
+//    println(user.getConnection())
+
+    checkDataTypes(user)
+
+}
+
+fun checkDataTypes(obj: MainProvider){
+    if(obj is UserInfoProvider){
+        obj.printInfo(User())
+    }
+    if(obj is DbConnection){
+        println(obj.getConnection())
+    }
 }
